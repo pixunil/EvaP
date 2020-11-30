1cd034d8f691016e371c5104009fd0c388f0d1f9
Jakob Stigloher <jakob.stigloher@student.hpi.de>
2020-11-30 21:02:37 +0100

    Add /staff/development/fixtures/<filename> route to view generated fixtures in the browser

--- evap/development/urls.py
+++ evap/development/urls.py
@@ -4,4 +4,5 @@ from evap.development import views
 
 urlpatterns = [
     path("components", views.development_components, name="development_components"),
+    path("fixtures/<path:filename>", views.development_fixtures, name="development_fixtures"),
 ]
